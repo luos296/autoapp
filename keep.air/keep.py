5859ea9785a6c98104f1b01029f842da3a4a072d
luo <296760222@qq.com>
2023-08-06 09:52:28 +0800

    增加日常任务

--- keep.air/keep.py
+++ keep.air/keep.py
@@ -3,7 +3,7 @@ __author__ = "luo"
 
 from airtest.core.api import *
 
-auto_setup(__file__, ['Android://127.0.0.1:5037/192.168.100.229:5555'], True)
+auto_setup(__file__, ['Android://127.0.0.1:5037/192.168.100.229:5555'])
 
 wake()
 stop_app('com.iplay.potatoclash.taptap')
